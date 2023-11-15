c5e9464d484bcf334deb2e54fb4538cba440667b
Lucas Bittencourt <lucasbbitten@gmail.com>
2023-11-14 23:54:15 -0500

    Fixing level manager adjustment

--- Assets/__Scripts/LevelManager.cs
+++ Assets/__Scripts/LevelManager.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Managers/Level Manager")]
 public class LevelManager : ScriptableObject
 {
-    public float m_levelSize;
+    [Tooltip("Size of the level to be adjusted")]
+    [SerializeField] float m_levelSize;
 
     Switches[] m_levelSwitches;
     CubeMovementController m_mainCube;
@@ -15,19 +16,18 @@ public class LevelManager : ScriptableObject
     List<GameObject> m_faces = new List<GameObject>();
     List<GameObject> m_forceFieldFaces = new List<GameObject>();
 
-    public float m_position;
-
     public void SetMainCube(CubeMovementController cube)
     {
         m_mainCube = cube;
+
+        m_faces.Clear();
+        m_forceFieldFaces.Clear();
 
         for (int i = 0; i < m_mainCube.Faces.Count; i++)
         {
             m_faces.Add(m_mainCube.Faces[i].gameObject);
             m_forceFieldFaces.Add(m_forceField.faces[i]);
         }
-
-        m_position = m_levelSize * 5;
 
         m_levelSwitches = FindObjectsOfType<Switches>();
 
@@ -56,63 +56,69 @@ public class LevelManager : ScriptableObject
 
     public void AdjustLevel()
     {
-        for (int i = 0; i < m_faces.Count; i++)
+        var mainCube = FindObjectOfType<CubeMovementController>();
+        var forceFieldCube = mainCube.GetComponentInChildren<ForceFieldCube>();
+
+        if (mainCube != null && forceFieldCube != null)
         {
-            Vector3 pos = m_faces[i].transform.localPosition;
+            SetForceFieldCube(forceFieldCube);
+            SetMainCube(mainCube);
+            mainCube.SetLevelSize(m_levelSize);
 
-            m_faces[i].transform.localScale = Vector3.one * m_levelSize;
-            m_forceFieldFaces[i].transform.localScale = new Vector3 (m_levelSize*10, 0.05f, m_levelSize*10);
+            for (int i = 0; i < m_faces.Count; i++)
+            {
+                Vector3 pos = m_faces[i].transform.localPosition;
 
-            if (Mathf.Abs(pos.x) > Mathf.Abs(pos.y) && Mathf.Abs(pos.x) > Mathf.Abs(pos.z))
-            {                    
-                if (pos.x > 0)
+                m_faces[i].transform.localScale = Vector3.one * m_levelSize;
+                m_forceFieldFaces[i].transform.localScale = new Vector3(m_levelSize * 10, 0.05f, m_levelSize * 10);
+
+                if (Mathf.Abs(pos.x) > Mathf.Abs(pos.y) && Mathf.Abs(pos.x) > Mathf.Abs(pos.z))
                 {
-                    m_faces[i].transform.localPosition = Vector3.right * m_levelSize * 5;
-                    m_forceFieldFaces[i].transform.localPosition = Vector3.right * m_levelSize * 5 - new Vector3 (0.02f,0,0);
+                    if (pos.x > 0)
+                    {
+                        m_faces[i].transform.localPosition = Vector3.right * m_levelSize * 5;
+                        m_forceFieldFaces[i].transform.localPosition = Vector3.right * m_levelSize * 5 - new Vector3(0.02f, 0, 0);
+                    }
+                    else
+                    {
+                        m_faces[i].transform.localPosition = Vector3.left * m_levelSize * 5;
+                        m_forceFieldFaces[i].transform.localPosition = Vector3.left * m_levelSize * 5 + new Vector3(0.02f, 0, 0);
+                    }
+                }
+                else if (Mathf.Abs(pos.y) > Mathf.Abs(pos.z))
+                {
+
+                    if (pos.y > 0)
+                    {
+                        m_faces[i].transform.localPosition = Vector3.up * m_levelSize * 5;
+                        m_forceFieldFaces[i].transform.localPosition = Vector3.up * m_levelSize * 5 - new Vector3(0, 0.02f, 0);
+
+                    }
+                    else
+                    {
+                        m_faces[i].transform.localPosition = Vector3.down * m_levelSize * 5;
+                        m_forceFieldFaces[i].transform.localPosition = Vector3.down * m_levelSize * 5 + new Vector3(0, 0.02f, 0);
+
+                    }
                 }
                 else
                 {
-                    m_faces[i].transform.localPosition = Vector3.left * m_levelSize * 5;
-                    m_forceFieldFaces[i].transform.localPosition = Vector3.left * m_levelSize * 5 + new Vector3 (0.02f,0,0);
-                }
-            } 
-            else if (Mathf.Abs(pos.y) > Mathf.Abs(pos.z))
-            {
+                    if (pos.z > 0)
+                    {
+                        m_faces[i].transform.localPosition = Vector3.forward * m_levelSize * 5;
+                        m_forceFieldFaces[i].transform.localPosition = Vector3.forward * m_levelSize * 5 - new Vector3(0, 0, 0.02f);
 
-                if (pos.y > 0)
-                {
-                    m_faces[i].transform.localPosition = Vector3.up * m_levelSize * 5;
-                    m_forceFieldFaces[i].transform.localPosition = Vector3.up * m_levelSize * 5 - new Vector3 (0,0.02f,0);
 
-                } 
-                else
-                {
-                    m_faces[i].transform.localPosition = Vector3.down * m_levelSize * 5;
-                    m_forceFieldFaces[i].transform.localPosition = Vector3.down * m_levelSize * 5 + new Vector3 (0,0.02f,0);
-
+                    }
+                    else
+                    {
+                        m_faces[i].transform.localPosition = Vector3.back * m_levelSize * 5;
+                        m_forceFieldFaces[i].transform.localPosition = Vector3.back * m_levelSize * 5 + new Vector3(0, 0, 0.02f);
+                    }
                 }
             }
-            else
-            {
-                if (pos.z > 0)
-                {
-                    m_faces[i].transform.localPosition = Vector3.forward * m_levelSize * 5;
-                    m_forceFieldFaces[i].transform.localPosition = Vector3.forward * m_levelSize * 5 - new Vector3 (0,0,0.02f);
 
-
-                } else
-                {
-                    m_faces[i].transform.localPosition = Vector3.back * m_levelSize * 5;
-                    m_forceFieldFaces[i].transform.localPosition = Vector3.back * m_levelSize * 5 + new Vector3 (0,0,0.02f);
-                }
-            }
         }
-
-        m_position = m_levelSize * 5;
-        Debug.Log("Ajustou o level");
     }
-
-
-
 
 }
